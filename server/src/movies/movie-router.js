a6b979d51dbf2d08adfe7bff02f2706c5078ebb5
Marko <markogajiin@gmail.com>
2020-07-13 01:34:56 +0200

    protect & authorize admin routes

--- server/src/movies/movie-router.js
+++ server/src/movies/movie-router.js
@@ -1,8 +1,10 @@
+import { protect, authorize } from '../middlewares/auth'
+
 export default function makeMovieRouter({ moviesEndpointHandler, router }) {
-    router.post('/', moviesEndpointHandler.addMovie)
+    router.post('/', protect, authorize, moviesEndpointHandler.addMovie)
     router.get('/', moviesEndpointHandler.getMovies)
     router.get('/:movie', moviesEndpointHandler.getMovie)
-    router.get('/imdb/:movie', moviesEndpointHandler.searchImdb)
+    router.get('/imdb/:movie', protect, authorize, moviesEndpointHandler.searchImdb)
     
     return router
 }
